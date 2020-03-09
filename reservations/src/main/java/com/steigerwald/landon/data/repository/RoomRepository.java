a080546e9caa96120a9a024549e14dff891e81d7
chris steigerwald <cjsteigerwald@gmail.com>
2020-03-09 02:08:11 -0700

    Adding reservations a hotel reservation system in java spring

---
+++ reservations/src/main/java/com/steigerwald/landon/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.steigerwald.landon.data.repository;
+
+import com.steigerwald.landon.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+    Room findByNumber(String number);
+}
